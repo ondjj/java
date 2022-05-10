29a4fa40eca17ee6edc630ea2546d07c6a1a1477
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-05-10 15:14:55 +0900

    220510

---
+++ chap03/src/verify/prob10/pie.java
@@ -0,0 +1,16 @@
+package verify.prob10;
+
+public class pie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1 = 10;
+		int var2 = 3;
+		int var3 = 14;
+		
+		double var4 = var1 * var1 * Double.parseDouble(var2 + "." + var3);
+		System.out.println("¿øÀÇ ³ĞÀÌ:" + var4);
+
+	}
+
+}
