4d9963e805434eed9eb6cc58a8b4fef940b4805d
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-05-13 23:55:01 +0900

    220513-2

---
+++ chap06/src/sec03/prob02/MemberObject.java
@@ -0,0 +1,14 @@
+package sec03.prob02;
+
+public class MemberObject {
+	
+	String name;
+	String id;
+	
+	MemberObject(String name, String id) {
+		this.name = name;
+		this.id = id;
+		
+	}
+
+}
---
+++ chap06/src/sec03/prob02/MemberObjectExample.java
@@ -0,0 +1,14 @@
+package sec03.prob02;
+
+public class MemberObjectExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MemberObject member = new MemberObject("ȫ�浿", "hong");
+		System.out.print(member.name);
+		System.out.print(member.id);
+
+	}
+
+}
