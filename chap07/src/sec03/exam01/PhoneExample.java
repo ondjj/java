2da3cd19ec2849850bfafa0681ed89241c000221
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-05-16 23:01:49 +0900

    220516-2

---
+++ chap07/src/sec03/exam01/PhoneExample.java
@@ -0,0 +1,17 @@
+package sec03.exam01;
+
+public class PhoneExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Phone phone = new phone();
+		
+		SmartPhone smartPhone = new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+
+	}
+
+}
---
+++ chap07/src/sec03/exam01/SmartPhone.java
@@ -0,0 +1,14 @@
+package sec03.exam01;
+
+public class SmartPhone extends Phone{
+	//creater1
+	public SmartPhone(String owner) {
+		super(owner);
+	}
+	
+	//method
+	public void internetSearch() {
+		System.out.println("인터넷 검색을 합니다.");
+	}
+
+}
