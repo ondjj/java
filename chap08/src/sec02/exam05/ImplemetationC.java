d976eda1f0645eeb0fc328985e85eca839741823
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-05-17 21:50:34 +0900

    220517-2

---
+++ chap08/src/sec02/exam05/ImplemetationC.java
@@ -0,0 +1,16 @@
+package sec02.exam05;
+
+public class ImplemetationC implements InterfaceC{
+	public void methodA() {
+		System.out.println("Implementation-methodA() 실행");
+	}
+	
+	public void methodB() {
+		System.out.println("Implementation-methodB() 실행");
+	}
+	
+	public void methodC() {
+		System.out.println("Implementation-methodC() 실행");
+	}
+
+}
