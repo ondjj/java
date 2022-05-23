92ab0421e423b2ba672a86f722599cb8b8a24e4d
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-05-23 18:05:54 +0900

    20220523-1

---
+++ chap11/src/sec01/exam27/MathRandomExample.java
@@ -0,0 +1,12 @@
+package sec01.exam27;
+
+public class MathRandomExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = (int) (Math.random()*6) + 1;
+		System.out.println("ÁÖ»çÀ§ ´«: " + num);
+
+	}
+
+}
