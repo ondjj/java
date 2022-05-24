36281cb5cdd8d668a2cb19809a70d8d853170dec
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-05-24 21:28:47 +0900

    220524-2

---
+++ chap12/src/sec02/exam05/DaemonExample.java
@@ -0,0 +1,18 @@
+package sec02.exam05;
+
+public class DaemonExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setDaemon(true); // autosavethread를 데몬으로 만듦
+		autoSaveThread.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {}
+		System.out.println("메인 스레드 종료");
+
+	}
+
+}
