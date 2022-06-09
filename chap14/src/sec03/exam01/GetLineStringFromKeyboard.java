a72fc51a911a72e2f81f71a98b8f6abfa9877498
Chang-Jun Jeon <86146128+ondjj@users.noreply.github.com>
2022-06-09 14:52:01 +0900

    220609

---
+++ chap14/src/sec03/exam01/GetLineStringFromKeyboard.java
@@ -0,0 +1,26 @@
+package sec03.exam01;
+
+import java.io.*;
+
+public class GetLineStringFromKeyboard {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		InputStream is = System.in;
+		Reader reader = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(reader);
+		
+		while(true) {
+			System.out.print("입력하세요: ");
+			String lineStr = br.readLine();
+			if(lineStr.equals("q") || lineStr.equals("quit")) break;
+			System.out.print("입력된 내용: " + lineStr);
+			System.out.println();
+		}
+		
+		br.close();
+
+	}
+
+}
