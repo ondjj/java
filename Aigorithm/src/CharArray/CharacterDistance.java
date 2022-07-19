e89508cd0bbcc0f60e473a19995d72a9bf2948c5
ondjj <jcjk0302@gmail.com>
2022-07-19 16:47:34 +0900

    inflearn

--- Aigorithm/src/CharArray/CharacterDistance.java
+++ Aigorithm/src/CharArray/CharacterDistance.java
@@ -1,5 +1,6 @@
 package CharArray;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class CharacterDistance {
@@ -16,49 +17,45 @@ public class CharacterDistance {
 		 * 
 		 * 출력 첫 번째 줄에 각 문자열 s의 각 문자가 문자 t와 떨어진 거리를 순서대로 출력한다.
 		 * 
-		 * 예시 입력 1
-		 * teachermode	te ac hermo de
-		 * 10 1210 12210
-		 * 예시 출력 1
-		 * 1 0 1 2 1 0 1 2 2 1 0
+		 * 예시 입력 1 teachermode 10123012340 예시 출력 1 1 0 1 2 1 0 1 2 2 1 0
 		 * 
-		 * 검증 1 .
-		 * 왼쪽 cnt와 오른쪽 cnt 비교 후 더 작은 값을 길이로 출력
+		 * 검증 1 . 왼쪽 cnt와 오른쪽 cnt 비교 후 더 작은 값을 길이로 출력
 		 */
-		
+
 		Scanner in = new Scanner(System.in);
-		
-		
-		char[] strCh = in.next().toCharArray();
-		char[] ch = in.next().toCharArray();
-		
-		int[] awswer = {strCh.length};
-		int taget = 1000;
-		
-		for (int i = 0; i < awswer.length; i++) {
-			if(strCh[i] != ch[i]) {
-				awswer[i] = taget;
-				taget ++ ;
-			}else {
-				taget = 0;
-				awswer[i] = taget;
-				taget ++ ;
+
+		String s = in.next();
+
+		char t = in.next().charAt(0);
+
+		int[] answer = new int[s.length()];
+		int p = 1000;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == t) {
+				p = 0;
+				answer[i] = p;
+			} else {
+				p++;
+				answer[i] = p;
 			}
-			System.out.print(awswer[i]);
+
 		}
-		
-		for (int i = awswer.length-1 ; i >= 0; i--) {
-			if(strCh[i] != ch[i]) {
-				awswer[i] = taget;
-				taget ++ ;
-			}else {
-				taget = 0;
-				awswer[i] = taget;
-				taget ++ ;
+		p = 1000;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) == t)
+				p = 0;
+			else {
+				p++;
+//				answer[i] = Math.min(answer[i], p);
+				if(answer[i]>p) {
+					answer[i] = p;
+				} 
 			}
 		}
-		for (int i : awswer) {
-			System.out.println(i+" ");
+
+		for (int i : answer) {
+			System.out.print(i + " ");
 		}
 
 	}
--- Aigorithm/src/sort/BubbleSort.java
+++ Aigorithm/src/sort/BubbleSort.java
@@ -4,19 +4,20 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {254,3,213,64,75,56,4,324,65,78,9,5,76,3410,8,342,76};
+		int[] a = { 254, 3, 213, 64, 75, 56, 4, 324, 65, 78, 9, 5, 76, 3410, 8, 342, 76 };
 		int temp;
-		for(int i = 1 ; i < a.length ; i ++) {
-			for(int j = 0 ; j < a.length -i -1 ; j ++) {
-				if(a[j]>a[j+1]) {
+		
+		for (int i = 1; i < a.length; i++) {
+			for (int j = 0; j < a.length - i - 1; j++) {
+				if (a[j] > a[j + 1]) {
 					temp = a[j];
-					a[j] = a[j+1];
-					a[j+1] = temp;
+					a[j] = a[j + 1];
+					a[j + 1] = temp;
 				}
 			}
 		}
-		
-		for(int i = 0 ; i < a.length ; i ++) {
+
+		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i] + " ");
 		}
 
